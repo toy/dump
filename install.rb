cd4d3624693812619efb235933d62bed8edc47df
Boba Fat <ivan@workisfun.ru>
2009-02-27 20:10:38 +0300

    forgot about assets.example & fixed install.rb

--- install.rb
+++ install.rb
@@ -1,9 +1,9 @@
 require 'fileutils'
 
-assets_template = File.join(File.dirname(__FILE__), 'assets')
-assets_config = 'config/assets'
+assets_template = File.join(File.dirname(__FILE__), 'assets.example')
+assets_config = File.join(RAILS_ROOT, 'config', 'assets')
 
 unless File.exists?(assets_config)
+  FileUtils.copy_file(assets_template, assets_config)
   puts "Created blank assets file"
-  FileUtils.cp(assets_template, assets_config)
 end
