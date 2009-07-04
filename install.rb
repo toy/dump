1592b74c44ecb4fe78666987081f3ef58da5db0b
Boba Fat <ivan@workisfun.ru>
2009-07-04 20:08:41 +0400

    using pathname to copy assets

--- install.rb
+++ install.rb
@@ -1,9 +1,9 @@
-require 'fileutils'
+require 'pathname'
 
-assets_template = File.join(File.dirname(__FILE__), 'assets.example')
-assets_config = File.join(RAILS_ROOT, 'config', 'assets')
+assets_template = Pathname(__FILE__) + 'assets.example'
+assets_config = Pathname(RAILS_ROOT) + 'config' + 'assets'
 
-unless File.exists?(assets_config)
-  FileUtils.copy_file(assets_template, assets_config)
+unless assets_config.exists?
+  File.copy(assets_template, assets_config, true)
   puts "Created blank assets file"
 end
