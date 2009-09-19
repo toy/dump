e1905ca9346f285ed89eff59892851ca9a83aae1
Boba Fat <ivan@workisfun.ru>
2009-09-20 00:09:59 +0400

    moved spec log to base app log folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,7 @@ Spec::Runner.configure do |config|
 end
 
 PLUGIN_SPEC_DIR = File.expand_path(File.dirname(__FILE__)) unless defined? PLUGIN_SPEC_DIR
-ActiveRecord::Base.logger = Logger.new(PLUGIN_SPEC_DIR + "/debug.log")
+ActiveRecord::Base.logger = Logger.new(File.join(RAILS_ROOT, 'log', 'dump-plugin-debug.log'))
 
 DUMMY_SCHEMA_PATH = File.join(PLUGIN_SPEC_DIR, "db", "schema.rb") unless defined? DUMMY_SCHEMA_PATH
 
