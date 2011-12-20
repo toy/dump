b776c0534c28285753954b1ab3a8521b9c05df8c
Gun.io Whitespace Robot <contact@gun.io>
2011-12-19 19:15:36 -0500

    Remove whitespace [Gun.io WhitespaceBot]

--- lib/shell_escape.rb
+++ lib/shell_escape.rb
@@ -1,10 +1,10 @@
 # escape.rb - escape/unescape library for several formats
 #
 # Copyright (C) 2006,2007 Tanaka Akira  <akr@fsij.org>
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #  1. Redistributions of source code must retain the above copyright notice, this
 #     list of conditions and the following disclaimer.
 #  2. Redistributions in binary form must reproduce the above copyright notice,
@@ -12,7 +12,7 @@
 #     and/or other materials provided with the distribution.
 #  3. The name of the author may not be used to endorse or promote products
 #     derived from this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
 # WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 # MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
