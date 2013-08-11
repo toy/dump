46146d20433009f22b8b73d2e39a00138f729e7c
Ivan Kuchin <tadump+git@gmail.com>
2013-08-11 03:27:59 +0200

    replaced complete app spec/dummy-3.1.3 with minimalistic dummy apps for rails 2.3, 3.0, 3.1, 3.2, 4.0

---
+++ spec/dummy-3.2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy32::Application.config.secret_token = '717cef941e2dcfa5ae61c687afcc51707db33cb3180798baa99fa33d663a2faecf464034a0da10dd905db1fb8975f74f096b0a8c7a6bde137805db3875bbbc97'
