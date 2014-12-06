a33e8b29958cd02c19168b26b188dbb032e1f3c5
Ivan Kuchin <tadump+git@gmail.com>
2014-12-06 21:44:32 +0100

    use %w[] where interpolation nod needed

--- lib/dump_rake/dump.rb
+++ lib/dump_rake/dump.rb
@@ -93,7 +93,7 @@ class DumpRake
       number = size
       return nil if number.nil?
       degree = 0
-      symbols = %W[B K M G T]
+      symbols = %w[B K M G T]
       while number >= 1000 && degree < symbols.length - 1
         degree += 1
         number /= 1024.0
