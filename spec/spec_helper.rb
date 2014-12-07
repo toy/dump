46c2a455cd4e666f04053ab4faef46e27e300dcd
Ivan Kuchin <tadump+git@gmail.com>
2014-12-07 03:46:54 +0100

    extract loading dummy rails app to spec/dummy_rails_app.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,40 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
 
-require 'rails/version'
-
-rails_version = Rails::VERSION::STRING.split('.')
-
-if rails_version[0].to_i < 3 && !Gem.respond_to?(:source_index)
-  module Gem
-    def self.source_index; sources; end
-    SourceIndex = Specification
-  end
-end
-
-environment_paths = (1..rails_version.length).map do |count|
-  version_part = rails_version[0, count].join('.')
-  File.expand_path("../dummy-#{version_part}/config/environment", __FILE__)
-end.reverse
-
-environment_paths.any? do |environment_path|
-  if File.exist?("#{environment_path}.rb")
-    require environment_path
-    true
-  end
-end || begin
-  command = "RAILS_VERSION=#{rails_version.join('.')} bundle exec rails " + if rails_version[0].to_i < 3
-    "spec/dummy-#{rails_version[0, 2].join('.')}"
-  else
-    "new spec/dummy-#{rails_version[0, 2].join('.')} -TSJ --skip-bundle"
-  end
-
-  abort [
-    "No dummy app for rails version #{rails_version.join('.')}",
-    "Create using `#{command}`",
-    'Tried:', *environment_paths
-  ].join("\n")
-end
-
+require 'dummy_rails_app'
 require 'dump_rake'
 
 PLUGIN_SPEC_DIR = File.expand_path(File.dirname(__FILE__)) unless defined? PLUGIN_SPEC_DIR
